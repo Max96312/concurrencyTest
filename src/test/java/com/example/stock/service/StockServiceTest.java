21ab2eb1b187834b59b6f5bb4cf5ef338b7f2740
Max96312 <k0mp4ny16@gmail.com>
2024-05-22 01:04:19 +0900

    문제점: race condition 발생

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -3,10 +3,15 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -40,5 +45,31 @@ class StockServiceTest {
         //then
         assertEquals(99, stock.getQuantity());
 
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        //given
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                }finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        //when
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        ///then
+        Assertions.assertEquals(0, stock.getQuantity());
     }
 }
