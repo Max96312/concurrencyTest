faef0ebb1a3b74a1f5a7f82321f4efac4c0e7fe0
Max96312 <k0mp4ny16@gmail.com>
2024-05-22 01:22:43 +0900

    OptimisticLock를 통한 문제 해결

--- src/main/java/com/example/stock/domain/Stock.java
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -1,9 +1,6 @@
 package com.example.stock.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,6 +15,9 @@ public class Stock {
 
     @Getter
     private Long quantity;
+
+    @Version
+    private Long version;
 
     public Stock(Long productId, Long quantity) {
         this.productId = productId;
